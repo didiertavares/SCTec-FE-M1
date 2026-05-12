38294e09cf54299f68797a491c55f386118a3854
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-05-11 22:25:19 -0300

    S05 11/05 - códigos & arquivos

---
+++ S05/class-pizza.js
@@ -0,0 +1,35 @@
+const input = input('prompt-sync')
+
+let borda
+
+calcularPreco(tamanho, borda) {
+   if(tamanho==P) {
+        console.log('o preço é R$ 25')
+    } else if(tamanho==M) {
+        console.log('o preço é R$ 35')
+    } else if(tamanho==G) {
+        console.log('o preço é R$ 50')
+    }
+
+
+class Pizza {
+    constructor(sabor, tamanho, borda) {
+        this.sabor = sabor
+        this.tamanho = tamanho
+        this.borda = borda
+    }
+    opcaoTamanho() {
+        console.log(`vc escolheu a pizza ${tamanho}`)
+    }
+    temBorda() {
+        if borda = true
+        console.log('tem um acréscimo de R$ 8')
+    }
+    }
+    resumo(){
+
+    }
+}
+
+// não terminado e provavelment não funciona
+//  > refazer
