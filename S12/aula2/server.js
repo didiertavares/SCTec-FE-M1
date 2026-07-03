acad6ed71085d21c5df9018979915e7939d30bdd
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2026-07-03 00:23:01 -0300

    S12 | aula2 > common.js X module; atualizar integrald código Quiz Copa

--- S12/aula2/server.js
+++ S12/aula2/server.js
@@ -1,38 +1,41 @@
+// essa forma abaixo ("require") é de Common.js; é considerada obsoleta no uso de Node.js.
 // const prompt = require("prompt-sync")();
 // const chalk = require("chalk");
 
+// abaixo, forma + moderna de tratar importação de libs e dados:
+// para tanto, é preciso trocar type: Common.js por module no arquivo package.json 
 import promptSync from "prompt-sync";
+
+// elimino a versão 4.1.2 incompatível da lib chalk pelo terminal: npm remove chalk
+// e reinstala-se sem especificar versão -> última versão por padrão
 import chalk from "chalk";
 
-import perguntas from "./perguntas.js";
+// import perguntas from "./perguntas.js";
 
 const prompt = promptSync();
 
-// const resposta = prompt("Qual o seu nome? ");
+const resposta = prompt("Qual o seu nome? ");
+console.log(chalk.bold.blue(resposta));
 
-// const resposta = prompt("Nome: ");
+// let acertos = 0;
 
-// console.log(chalk.bold.blue(resposta));
+// for (let i = 0; i < perguntas.length; i++) {
+//   const questao = perguntas[i];
 
-let acertos = 0;
+//   console.log(chalk.bold.blue("\n" + questao.pergunta));
+//   console.log(chalk.bold.hex("#FFA500")(questao.alternativas.join("    ")));
 
-for (let i = 0; i < perguntas.length; i++) {
-  const questao = perguntas[i];
+//   const resposta = prompt("Qual a sua resposta (número)?");
 
-  console.log(chalk.bold.blue("\n" + questao.pergunta));
-  console.log(chalk.bold.hex("#FFA500")(questao.alternativas.join("    ")));
+//   if (Number(resposta) === questao.correta) {
+//     console.log(chalk.bold.green("Acertou!"));
+//     acertos++;
+//   } else {
+//     console.log(chalk.bold.red("Errou!"));
+//   }
 
-  const resposta = prompt("Qual a sua resposta (número)?");
+//   // console.log(questao);
+// }
 
-  if (Number(resposta) === questao.correta) {
-    console.log(chalk.bold.green("Acertou!"));
-    acertos++;
-  } else {
-    console.log(chalk.bold.red("Errou!"));
-  }
-
-  // console.log(questao);
-}
-
-console.log("Você acertou " + acertos + " perguntas!");
-// export const perguntas = [...]
+// console.log("Você acertou " + acertos + " perguntas!");
+// // export const perguntas = [...]
